1f979288f51f157faa53a40130c6bf00cd6da13e
Sumayya Syeda <sumayyasyeda@iMac.local>
2021-01-09 21:21:49 -0800

    app udpate

--- app.py
+++ app.py
@@ -48,3 +48,9 @@ def processRequest(res):
     else:
         log.write_log(sessionID, "Bot Says: " + result.fulfillmentText)
 
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
+
+
